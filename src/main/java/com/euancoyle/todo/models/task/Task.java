4ba8f7e2b1cb53f0973397a5086243bd842e41db
Ecoyle33 <92370561+Ecoyle33@users.noreply.github.com>
2025-05-14 19:37:57 +0100

    Added Hibernate to pom.xml, updated POJO annotations

--- src/main/java/com/euancoyle/todo/models/task/Task.java
+++ src/main/java/com/euancoyle/todo/models/task/Task.java
@@ -1,10 +1,12 @@
 package com.euancoyle.todo.models.task;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Task {
 
     private int id;
--- src/main/java/com/euancoyle/todo/models/user/User.java
+++ src/main/java/com/euancoyle/todo/models/user/User.java
@@ -1,10 +1,12 @@
 package com.euancoyle.todo.models.user;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class User {
 
     private int id;
